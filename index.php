8a79434789d5364b9e340c9990e93a14803b5cdb
mbs0027 <mbs0027@mix.wvu.edu>
2021-02-05 21:06:28 -0500

    Lab 2 about page

---
+++ index.php
@@ -0,0 +1,52 @@
+<main>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>About Us</h1>
+        <p>We are a new company created to give you to best and most accurate game reviews. Our CEO is one of the most avid gameers on the planet. 
+            He started the company to give people real game reviews not ones that are written by companies that get paid to write them. His vision is to write reviews that actually tell you what is good and bad about each game. 
+            Our Director Rick is the smartest man in the multiverse so of course he knows a lot about video games. The Art Director Peter Griffin is one of the greatest artists in modern history (we just hired him for the laughs). Also, Morty is here just to yell at when something goes wrong. We at (insert company name) will strive to give you the greatest reviews possible.</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/1.jpg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Matt Spickler</p>
+                    <p>Email: mbs0027@mix.wvu.edu</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/2.jpg">
+                <div class="container">
+                    <h2>Director</h2>
+                    <p>Rick Sanchez</p>
+                    <p>Email: rickc137@wubbalubbadubdub.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/3.jpg">
+                <div class="container">
+                    <h2>Grandson</h2>
+                    <p>Morty Smith</p>
+                    <p>Email: mortyxjessica@awgeez.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/4.jpg">
+                <div class="container">
+                    <h2>Art Director</h2>
+                    <p>Peter Griffin</p>
+                    <p>Email: peteristhebest@chris.is.fat.edu</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
